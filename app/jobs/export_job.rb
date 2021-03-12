dc2c20379813bd3ef07f1319d1900d19b9a76d08
maatinito <15379878+maatinito@users.noreply.github.com>
2021-03-11 14:41:32 -1000

    Fix exception when ExportJob is launched by cron

--- app/jobs/export_job.rb
+++ app/jobs/export_job.rb
@@ -1,11 +1,11 @@
 # frozen_string_literal: true
 
 class ExportJob < CronJob
-  self.schedule_expression = 'every weekday at 07:00 and 13:00'
+  self.schedule_expression = 'every weekday at 12:00 and 19:00'
 
   MANUAL_SYNC = 'ManualSync'
 
-  def perform(reset, config)
+  def perform(reset: false, config: 'storage/demarches.yml')
     Sync.find_or_create_by(job: MANUAL_SYNC)
     Sync.find_or_create_by(job: self.class.name) do
       DemarcheService.new(reset: reset, config_file: config).process
@@ -20,9 +20,9 @@ class ExportJob < CronJob
   end
 
   class << self
-    def run(reset, debug)
+    def run(reset, config)
       Sync.find_or_create_by(job: MANUAL_SYNC) do
-        ExportJob.perform_later(reset, debug)
+        ExportJob.perform_later(reset: reset, config: config)
       end
     end
 

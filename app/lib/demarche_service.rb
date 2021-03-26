6f7a793fc76069edc97b0fba1ec5794ac4e97a7a
maatinito <15379878+maatinito@users.noreply.github.com>
2021-03-25 17:00:07 -1000

    Fix task_execution not deleted when dossier is no longer accessible

--- app/lib/demarche_service.rb
+++ app/lib/demarche_service.rb
@@ -83,9 +83,13 @@ class DemarcheService
     tasks.each(&:before_run)
     updated_task_execution_query(demarche, tasks)
       .group_by(&:dossier)
-      .each do |dossier, task_executions|
-      on_dossier(dossier) do |dossier|
-        apply_updated_tasks(dossier, task_executions, tasks)
+      .each do |dossier_nb, task_executions|
+      on_dossier(dossier_nb) do |dossier|
+        if dossier.present?
+          apply_updated_tasks(dossier, task_executions, tasks)
+        else
+          TaskExecution.where(dossier: dossier_nb).destroy_all
+        end
       end
     end
     tasks.each(&:after_run)

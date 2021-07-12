1269c6a69860f10ac4c22b78b8d37c168040b689
maatinito <15379878+maatinito@users.noreply.github.com>
2021-07-11 21:32:57 -1000

    Export_cis: candidats admins must be read in Iso8859-1 (UTF8 errors)

--- app/lib/cis/export_etat_reel.rb
+++ app/lib/cis/export_etat_reel.rb
@@ -36,7 +36,7 @@ module Cis
       file = dossier.annotations.select { |champ| champ.label == 'Candidats admis' }&.first&.file
       throw "Le dossier initial #{dossier.number} n'a pas de list de candidats admis" if file.nil?
       download(file.url, file.filename) do |f|
-        xlsx = Roo::Spreadsheet.open(f, csv_options: { col_sep: ';', encoding: Encoding::UTF_8 })
+        xlsx = Roo::Spreadsheet.open(f, csv_options: { col_sep: ';', encoding: Encoding::ISO_8859_1 })
         sheet = xlsx.sheet(0)
         start_date = Date.parse(sheet.cell(2, 2))
         start_month = start_date.month

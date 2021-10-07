fe4e8b6ceb32ec73df523ba24b09d67defc7478c
maatinito <15379878+maatinito@users.noreply.github.com>
2021-10-06 14:39:12 -1000

    Lazy load mailjet only if used

--- config/initializers/mailjet.rb
+++ config/initializers/mailjet.rb
@@ -1,8 +1,12 @@
 # frozen_string_literal: true
 
-Mailjet.configure do |config|
-  config.api_key = Rails.application.secrets.mailjet[:api_key]
-  config.secret_key = Rails.application.secrets.mailjet[:secret_key]
-  config.default_from = ENV.fetch('CONTACT_EMAIL', "mes-demarches#{64.chr}modernisation.gov.pf")
-  config.api_version = 'v3.1'
+ActiveSupport.on_load(:action_mailer) do
+  require 'mailjet'
+
+  Mailjet.configure do |config|
+    config.api_key = Rails.application.secrets.mailjet[:api_key]
+    config.secret_key = Rails.application.secrets.mailjet[:secret_key]
+    config.default_from = ENV.fetch('CONTACT_EMAIL', "mes-demarches#{64.chr}modernisation.gov.pf")
+    config.api_version = 'v3.1'
+  end
 end

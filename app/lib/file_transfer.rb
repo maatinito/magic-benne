35c2feb347bb0ba7b6e855e45100a9c960ca34ab
maatinito <15379878+maatinito@users.noreply.github.com>
2021-12-23 15:33:53 -1000

    Oblivacc: added ftp transfer

---
+++ app/lib/file_transfer.rb
@@ -0,0 +1,139 @@
+require 'net/ftp'
+
+class FileTransfer < DossierTask
+  include Utils
+
+  def version
+    super + 1
+  end
+
+  def required_fields
+    super + %i[serveur identifiant mot_de_passe taches]
+  end
+
+  def authorized_fields
+    super + %i[port]
+  end
+
+  def before_run
+    @has_new_dossier = false
+  end
+
+  def run
+    @has_new_dossier = true
+  end
+
+  def after_run
+    # return unless @has_new_dossier || ENV.fetch('FORCE_EXECUTION', nil)
+
+    host = params[:serveur]
+    Rails.logger.tagged(host) do
+      user = params[:identifiant]
+      password = params[:mot_de_passe]
+      port = params[:port] || '21'
+      @ftp = Net::FTP.new(host, port: port, username: user, password: password, ssl: port == '21')
+      execute_tasks
+    end
+  end
+
+  private
+
+  def execute_tasks
+    @tasks = params[:taches]
+    @tasks.each do |task|
+      case task.first[0]
+      when 'telecharger'
+        download(task)
+      when 'televerser'
+        upload(task)
+      when 'effacer'
+        delete(task)
+      else
+        throw "Instruction de transfer ftp inconnue #{task.first[0]}"
+      end
+    end
+  end
+
+  def download(task)
+    move_files = %w[true oui].include?(task['deplacer']&.downcase)
+    pattern = task.first[1]
+    path = File.dirname(pattern)
+    basename = File.basename(pattern)
+    to = task['vers'] || '.'
+    Rails.logger.debug((move_files ? 'Moving' : 'Downloading') + " #{pattern} to local #{to}")
+    begin
+      @ftp.chdir(path) if path.present?
+      @ftp.nlst(basename).each do |filename|
+        localfile = "#{to}/#{filename}"
+        Rails.logger.debug("Downloading #{filename} to #{localfile}")
+        @ftp.getbinaryfile(filename, localfile)
+        delete_remote_file(filename, localfile, pattern) if move_files
+      end
+    rescue StandardError => e
+      log_error("Error downloading #{pattern} from #{params[:serveur]}", e)
+    end
+  end
+
+  def log_error(message, e)
+    message = "#{message}: #{e.message}"
+    Rails.logger.error(message)
+    e.backtrace.first(15).each { |bt| Rails.logger.error(bt) }
+    NotificationMailer.with(message: message).report_error.deliver_later
+  end
+
+  def delete_remote_file(filename, localfile, pattern)
+    if File.new(localfile).size == @ftp.size(filename)
+      Rails.logger.debug("Deleting remote #{filename}")
+      @ftp.delete(filename)
+    else
+      message = "Problème sur le transfert du fichier #{filename} (#{pattern} sur #{params[:serveur]}}. " \
+                "Taille du fichier en local: #{File.new(localfile).size}. " \
+                "Taille du fichier distant: #{@ftp.size(filename)}"
+      Rails.logger.error(message)
+      NotificationMailer.with(message: message).report_error.deliver_later if @ftp.closed?
+    end
+  end
+
+  def upload(task)
+    move_files = %w[true oui].include?(task['deplacer']&.downcase)
+    pattern = task.first[1]
+    to = task['vers']
+    Rails.logger.debug((move_files ? 'Moving' : 'Uploading') + " #{pattern} to remote #{to}")
+    begin
+      @ftp.chdir(to) if to.present?
+      Dir.glob(pattern).each do |filename|
+        Rails.logger.debug("Uploading #{filename} to #{to}")
+        @ftp.putbinaryfile(filename)
+        delete_local_file(filename) if move_files
+      end
+    rescue StandardError => e
+      log_error("Error uploading #{pattern} to #{params[:serveur]}", e)
+    end
+  end
+
+  def delete_local_file(filename)
+    basename = File.basename(filename)
+    if File.new(filename).size == @ftp.size(basename)
+      Rails.logger.debug("Deleting local #{filename}")
+      File.delete(filename)
+    else
+      message = "Problème sur le transfert du fichier #{filename} (#{params[:serveur]}}. " \
+                "Taille du fichier en local: #{File.new(filename).size}. " \
+                "Taille du fichier distant: #{@ftp.size(basename)}"
+      Rails.logger.error(message)
+      NotificationMailer.with(message: message).report_error.deliver_later if @ftp.closed?
+    end
+  end
+
+  def delete(task)
+    pattern = task.first[1]
+    path = File.dirname(pattern)
+    basename = File.basename(pattern)
+    @ftp.chdir(path) if path.present?
+    Rails.logger.debug("Deleting remote pattern #{pattern}")
+    @ftp.nlst(basename).each do |filename|
+      Rails.logger.debug("Deleting remote #{filename}")
+      @ftp.delete(filename)
+    end
+  end
+end
---
+++ spec/lib/file_transfer_spec.rb
@@ -0,0 +1,91 @@
+require 'rails_helper'
+require 'fileutils'
+require 'testftpd'
+
+describe FileTransfer do
+  let(:demarche_id) { 1008 }
+  let(:job) { { demarche: demarche_id, name: 'name' }}
+  let(:server) { '127.0.0.1' }
+  let(:login) { 'username' }
+  let(:password) { 'password' }
+  let(:tasks) { {} }
+  let(:root) { Rails.root / 'tmp/file_transfer' }
+  let(:ftp_root) { root / 'remote' }
+  let(:remote_dir) { ftp_root / 'input' }
+  let(:local_dir) { root / 'local' }
+  let(:port) { 2121 }
+  let(:ftp_server) { TestFtpd::Server.new(port: port, root_dir: ftp_root.to_s) }
+
+  subject { FileTransfer.new(job, { serveur: server, port: port, identifiant: login, mot_de_passe: password, taches: tasks}) }
+
+  before do
+    Demarche.new(id: demarche_id).save
+    puts remote_dir
+    puts local_dir
+    FileUtils.mkdir_p remote_dir.to_s
+    File.write(remote_dir / 'remote1.csv', 'CSV content')
+    File.write(remote_dir / 'remote2.csv', 'CSV content')
+    File.write(remote_dir / 'exclude_remote.csv', 'CSV content')
+    FileUtils.mkdir_p local_dir
+    File.write(local_dir / 'local1.csv', 'CSV content')
+    File.write(local_dir / 'exclude_local.csv', 'CSV content')
+    ftp_server.start unless ftp_server.running?
+    subject.before_run
+    subject.run
+    subject.after_run
+  end
+
+  after do
+    ftp_server.shutdown
+    FileUtils.rm_rf root
+  end
+
+  context 'files to download' do
+    let(:tasks) { [{ 'telecharger' => 'input/remote*.csv', 'vers' => local_dir }] }
+    it 'must be transfered' do
+      puts local_dir / 'remote1.csv'
+      expect(File).to exist(local_dir / 'remote1.csv')
+      expect(File).to exist(local_dir / 'remote2.csv')
+      expect(File).not_to exist(local_dir / 'exclude_remote.csv')
+      expect(File).to exist(remote_dir / 'remote1.csv')
+    end
+  end
+
+  context 'files to move from remote' do
+    let(:tasks) { [{ 'telecharger' => 'input/remote*.csv', 'vers' => local_dir, 'deplacer' => 'Oui' }] }
+    it 'must be transfered and deleted from remote' do
+      puts local_dir / 'remote1.csv'
+      expect(File).to exist(local_dir / 'remote1.csv')
+      expect(File).to exist(local_dir / 'remote2.csv')
+      expect(File).not_to exist(local_dir / 'exclude_remote.csv')
+      expect(File).not_to exist(remote_dir / 'remote1.csv')
+    end
+  end
+
+  context 'files to upload' do
+    let(:tasks) { [{ 'televerser' => local_dir / 'local*.csv', 'vers' => 'input', 'deplacer' => 'Non' }] }
+    it 'must be transfered' do
+      expect(File).to exist(remote_dir / 'local1.csv')
+      expect(File).to exist(local_dir / 'local1.csv')
+      expect(File).not_to exist(remote_dir / 'exclude_local.csv')
+    end
+  end
+
+  context 'files to move to remote' do
+    let(:tasks) { [{ 'televerser' => local_dir / 'local*.csv', 'vers' => 'input', 'deplacer' => 'Oui' }] }
+    it 'must be transfered' do
+      expect(File).to exist(remote_dir / 'local1.csv')
+      expect(File).not_to exist(local_dir / 'local1.csv')
+      expect(File).not_to exist(remote_dir / 'exclude_local.csv')
+    end
+  end
+
+  context 'files to delete' do
+    let(:tasks) { [{ 'effacer' => 'input/remote*.csv' }] }
+    it 'must be deleted' do
+      expect(File).not_to exist(remote_dir / 'remote1.csv')
+      expect(File).not_to exist(remote_dir / 'remote2.csv')
+      expect(File).to exist(remote_dir / 'exclude_remote.csv')
+    end
+  end
+end

36604d6e5030fb8f6d96666681c208da0fd6c482
Christian <15379878+maatinito@users.noreply.github.com>
2022-05-17 08:52:30 -1000

    rubocop

--- config/initializers/inflections.rb
+++ config/initializers/inflections.rb
@@ -41,14 +41,14 @@ ActiveSupport::Inflector.inflections(:fr) do |inflect|
   inflect.plural(/(bijou|caillou|chou|genou|hibou|joujou|pou|au|eu|eau)$/, '\1x')
   inflect.singular(/(bijou|caillou|chou|genou|hibou|joujou|pou|au|eu|eau)x$/, '\1')
 
-  inflect.plural(/(bleu|émeu|landau|lieu|pneu|sarrau)$/, '\1s')
+  inflect.plural(/(bleu|Ã©meu|landau|lieu|pneu|sarrau)$/, '\1s')
   inflect.plural(/al$/, 'aux')
   inflect.plural(/ail$/, 'ails')
   inflect.singular(/(journ|chev)aux$/, '\1al')
   inflect.singular(/ails$/, 'ail')
 
-  inflect.plural(/(b|cor|ém|gemm|soupir|trav|vant|vitr)ail$/, '\1aux')
-  inflect.singular(/(b|cor|ém|gemm|soupir|trav|vant|vitr)aux$/, '\1ail')
+  inflect.plural(/(b|cor|Ã©m|gemm|soupir|trav|vant|vitr)ail$/, '\1aux')
+  inflect.singular(/(b|cor|Ã©m|gemm|soupir|trav|vant|vitr)aux$/, '\1ail')
 
   inflect.plural(/(s|x|z)$/, '\1')
 

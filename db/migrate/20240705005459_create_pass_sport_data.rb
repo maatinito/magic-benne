29e36be697a52e2a803a4a52587072bc65c065bc
Christian Lautier <15379878+maatinito@users.noreply.github.com>
2024-07-05 07:01:33 -1000

    added pass'sport status task to return feedback on dossiers

---
+++ db/migrate/20240705005459_create_pass_sport_data.rb
@@ -0,0 +1,18 @@
+class CreatePassSportData < ActiveRecord::Migration[6.1]
+  def change
+    create_table :pass_sport_data do |t|
+        t.integer :dossier, null: false
+        t.string :siret, null: false
+        t.string :status
+        t.boolean :eligible
+        t.boolean :invoices_verified
+        t.string :cps_feedback_checksum
+
+        t.timestamps
+      end
+
+      # Ajout des index
+      add_index :pass_sport_data, :dossier, unique: true
+      add_index :pass_sport_data, :siret
+  end
+end
